2cba74263caf4a3ce59a529129804c74da124b35
Rebecca <tubman.rebecca@gmail.com>
2021-03-30 14:24:35 +0300

    Fixed settings.py and urls.py to work for 'home'
    A commit following the last 'Init home app' PR where 'landingpage'
    was changed to 'home' but had the files above relate to the previous
    name.
    
    modified:   shlifim/settings.py, shlifim/urls.py

--- shlifim/settings.py
+++ shlifim/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'landingpage.apps.LandingpageConfig',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
