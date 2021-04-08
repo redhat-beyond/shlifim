640637e327dbbd073e184d1be6b10c8523b654b5
Lior Noy <ironoy@gmail.com>
2021-04-08 20:16:32 +0300

    Change home admin.py file to unite profile and user
    This change will move the details of the profile into
    the same page as the User objects in the admin page

--- home/admin.py
+++ home/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from .models import Profile, Subject, Sub_Subject, Book
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
-class BookAdmin(admin.ModelAdmin):
-    model = Book
-    list_display = ("book_name", "related_subject")
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline, )
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
 
 
 class SubSubjectAdmin(admin.ModelAdmin):
@@ -12,7 +24,13 @@ class SubSubjectAdmin(admin.ModelAdmin):
     list_display = ("sub_subject_name", "related_subject")
 
 
-admin.site.register(Profile)
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = ("book_name", "related_subject")
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Subject)
 admin.site.register(Sub_Subject, SubSubjectAdmin)
 admin.site.register(Book, BookAdmin)
