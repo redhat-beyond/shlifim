258e586bbd2844cfc4a85a1626820fc1145aaac8
Danit Levi <danitLevi2708@gmail.com>
2021-04-22 17:19:52 +0300

    Adding tests to explore page
    -Testing "get_answers_num" method on question modek with multiple arguments
    -Testing explore page URL

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
-# from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.test import Client
+from .models import Question
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_explore_page_url():
+    url = reverse('explore-page')
+    c = Client()
+    response = c.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("question_id, expected_result", [
+    ("1", 1),
+    ("2", 1),
+    ("3", 0),
+    ("4", 0),
+])
+@pytest.mark.django_db
+def test_answers_num_func(question_id, expected_result):
+    question = Question.objects.get(id=question_id)
+    answers_num = question.get_answers_num()
+    assert answers_num == expected_result
