483658dfc22a13f8d828d6c596c2abbb18eee7fc
Ido <idokk96@gmail.com>
2021-05-10 23:54:43 +0300

    adding tests and change redirect to home page

--- shlifim/settings.py
+++ shlifim/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = 'about'
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
