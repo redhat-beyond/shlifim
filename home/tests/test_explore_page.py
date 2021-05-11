2bcef53e2be91ee92110e32f849e023ba491f998
Danit Levi <danitLevi2708@gmail.com>
2021-05-12 02:10:42 +0300

    Adding tests to explore page paginator
    - Creating class TestExplorePagePaginator in tests.py  file
     * Testing that "explore page" is paginated
     * Testing all questions appear in the "explore page" and corresponding
         the selected "items_per_page"

--- home/tests/test_explore_page.py
+++ home/tests/test_explore_page.py
@@ -1,9 +1,18 @@
 import pytest
 from home.models import Question
 from django.urls import reverse
+import math
 
 
 class TestExplorePage:
+
+    @pytest.mark.django_db
+    @pytest.fixture
+    def explore_page_response(self, client):
+        url = reverse('explore-page')
+        response = client.get(url)
+        return response
+
     class TestModelFunctions:
 
         @pytest.mark.parametrize("question_id, expected_result", [
@@ -21,16 +30,38 @@ class TestExplorePage:
     class TestExplorePageView:
 
         @pytest.mark.django_db
-        @pytest.fixture
-        def explore_page_response(self, client):
-            url = reverse('explore-page')
-            response = client.get(url)
-            return response
-
-        @pytest.mark.django_db
         def test_explore_page_response(self, explore_page_response):
             assert explore_page_response.status_code == 200
             assert explore_page_response.templates[0].name == "home/explore.html"
 
-            questions = Question.objects.all()
-            assert set(explore_page_response.context['questions']) == set(questions)
+    class TestExplorePagePaginator:
+        @pytest.mark.django_db
+        def test_explore_page_is_paginated(self, explore_page_response):
+            assert (explore_page_response.context['is_paginated'] is True)
+
+        @pytest.mark.parametrize("items_per_page", [
+            5,
+            10,
+            15,
+            20
+        ])
+        @pytest.mark.django_db
+        def test_explore_page_items_per_page(self, items_per_page, client):
+            # assert all questions appears in the explore page and corresponding the selected "items_per_page" .
+            questions = list(Question.objects.all())
+
+            last_page = math.ceil(len(questions) / items_per_page)
+            curr_page = 1
+            for i in range(0, len(questions), items_per_page):
+                response = client.get(
+                    reverse('explore-page') + '?paginate_by=' + str(items_per_page) + '&page=' + str(curr_page))
+                response_questions = list(response.context['questions'])
+                requested_questions = questions[i:i + items_per_page]
+
+                assert (response_questions == requested_questions)
+
+                if curr_page != last_page:
+                    assert (len(response_questions) == items_per_page)
+                    curr_page += 1
+                else:
+                    assert (len(response_questions) == len(questions) % items_per_page)
