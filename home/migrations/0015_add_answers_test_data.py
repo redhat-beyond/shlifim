6e16a3227e3d4f8facc1e263c376c47801c8a4c6
liornoy <ironoy@gmail.com>
2021-05-13 13:35:43 +0300

    Change in Answer models and migrations
    Answer model changes:
    * Change likes_count field (int) to likes (manytomany with Profile)
    * Remove 'thumb_up_answer' & ' thumb_down_answer' methods
    * Added handle_thumb_up and handle_thumb_down methods
    * Added boolean functions for checking if a user dis\likes an answer
    * Add get_answers_tuples class method
    
    Migration changes:
    * Changed the 'likes' field of Answer model in:
      migrations/0007_answer.py
    
    * Updated the test_data to support manytomany field in:
     migrations/0008_test_data_answer.py
     migrations/0015_add_answers_test_data.py

--- home/migrations/0015_add_answers_test_data.py
+++ home/migrations/0015_add_answers_test_data.py
@@ -12,30 +12,40 @@ class Migration(migrations.Migration):
         from home.models import Profile, Question, Answer
 
         answer_test_data = [(2, 1, 'This is an old Answer',
-                            datetime(2019, 5, 3, tzinfo=pytz.UTC), 1, 2, False),
+                            datetime(2019, 5, 3, tzinfo=pytz.UTC), [1], [2, 3], False),
                             (1, 1, 'This is an popular Answer',
-                            datetime(2021, 1, 3, tzinfo=pytz.UTC), 100, 3, False),
+                            datetime(2021, 1, 3, tzinfo=pytz.UTC), [1, 2, 3, 4, 5, 6], [], False),
                             (3, 1, 'The answer is 2 for sure. I have master degree in Math from Harvard',
-                            datetime(2021, 2, 14, tzinfo=pytz.UTC), 5, 1, False),
+                            datetime(2021, 2, 14, tzinfo=pytz.UTC), [], [], False),
                             (4, 1, '1+1 = 2. Simple algebra.',
-                            datetime(2021, 4, 14, tzinfo=pytz.UTC), 5, 1, False),
+                            datetime(2021, 4, 14, tzinfo=pytz.UTC), [3, 4], [1], False),
                             (2, 14, 'Old Answer',
-                            datetime(2019, 5, 3, tzinfo=pytz.UTC), 1, 2, False),
+                            datetime(2019, 5, 3, tzinfo=pytz.UTC), [1], [], False),
                             (1, 14, 'Popular Answer',
-                            datetime(2021, 1, 3, tzinfo=pytz.UTC), 100, 3, False),
+                            datetime(2021, 1, 3, tzinfo=pytz.UTC), [1, 2, 3, 4, 5, 6], [], False),
                             (3, 14, 'Answer A',
-                            datetime(2021, 2, 14, tzinfo=pytz.UTC), 5, 1, False),
+                            datetime(2021, 2, 14, tzinfo=pytz.UTC), [4], [1, 2, 3], False),
                             (4, 14, 'Answer B',
-                            datetime(2021, 4, 14, tzinfo=pytz.UTC), 5, 1, False),
+                            datetime(2021, 4, 14, tzinfo=pytz.UTC), [1, 2, 3], [5], False),
                             ]
 
         with transaction.atomic():
 
-            for user_id, question, content, publish_date, likes_count, dislikes_count, is_edited in answer_test_data:
+            for (user_id, question, content, publish_date, likes_profiles,
+                 dislike_profiles, is_edited) in answer_test_data:
                 curr_question = Question.objects.get(id=question)
                 profile = Profile.objects.get(user=user_id)
-                Answer(profile=profile, question=curr_question, content=content, publish_date=publish_date,
-                       likes_count=likes_count, dislikes_count=dislikes_count, is_edited=is_edited).save()
+                answer = Answer(profile=profile, question=curr_question, content=content, publish_date=publish_date,
+                                is_edited=is_edited)
+                answer.save()
+
+                if likes_profiles is not None:
+                    for profile_id in likes_profiles:
+                        answer.likes.add(profile_id)
+
+                if dislike_profiles is not None:
+                    for profile_id in dislike_profiles:
+                        answer.dislikes.add(profile_id)
 
     def create_tags_test_data(apps, schema_editor):
         from home.models import Tag, Question_Tag, Question
