b38f670e0fc6ea3b373638db4a967fa6a4365f60
Ido <idokk96@gmail.com>
2021-05-13 21:46:11 +0300

    create signup form
    squash
    
    fixview

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from .models import Question
+from .models import Question, Gender
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ('title', 'subject', 'sub_subject', 'grade', 'book', 'book_page', 'content')
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    gender = forms.CharField(label='Gender', widget=forms.Select(choices=Gender.choices), initial='U')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'gender', 'password1', 'password2', )
