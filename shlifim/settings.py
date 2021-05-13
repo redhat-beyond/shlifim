1aeaa040ef83e953c501ebe0bc64ba89169aed66
Ido <idokk96@gmail.com>
2021-05-13 11:49:56 +0300

    redirect to last page

--- shlifim/settings.py
+++ shlifim/settings.py
@@ -70,6 +70,14 @@ TEMPLATES = [
     },
 ]
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+)
+
 WSGI_APPLICATION = 'shlifim.wsgi.application'
 
 
