dc475b636f243992e48ceabaaf76ed99810494b3
Danit Levi <danitLevi2708@gmail.com>
2021-05-15 14:34:31 +0300

    Adding tests
    - Updating test_explore_page.py file:
      * Testing content received after GET requests with a tag parameter
        contains only qeuestions with this specific tag .
      * Testing context received after GET requests with a tag parameter
        contain this tag.
      * Testing context received after GET requests without a tag parameter
        contain None
      * Testing GET requests with a tag parameter that does not exist in
         the site raises 404 error
    
     Updating test_tags_page.py file :
      * Testing each tag contain url to the expxlore page with the requested
        tag

--- home/tests/test_explore_page.py
+++ home/tests/test_explore_page.py
@@ -4,6 +4,14 @@ from django.urls import reverse
 
 
 class TestExplorePage:
+
+    @pytest.mark.django_db
+    @pytest.fixture
+    def explore_page_response(self, client):
+        url = reverse('explore-page')
+        response = client.get(url)
+        return response
+
     class TestModelFunctions:
 
         @pytest.mark.parametrize("question_id, expected_result", [
@@ -21,13 +29,6 @@ class TestExplorePage:
     class TestExplorePageView:
 
         @pytest.mark.django_db
-        @pytest.fixture
-        def explore_page_response(self, client):
-            url = reverse('explore-page')
-            response = client.get(url)
-            return response
-
-        @pytest.mark.django_db
         def test_explore_page_response(self, explore_page_response):
             assert explore_page_response.status_code == 200
             assert explore_page_response.templates[0].name == "home/explore.html"
@@ -39,3 +40,47 @@ class TestExplorePage:
         def test_new_question_btn_in_explore_page(self, explore_page_response):
             char_content = explore_page_response.content.decode(explore_page_response.charset)
             assert '<a href="%s"' % reverse("new_question") in char_content
+
+    class TestTagsFilter:
+
+        @pytest.mark.parametrize(('tag_name', 'wanted_questions_ids_lst'), [
+            ("Bagrut_Exam", [8, 2, 14]),
+            ("5th_Grade", [4, 3, 1]),
+            ("Pitagoras", [8, 3, 1]),
+        ])
+        @pytest.mark.django_db
+        def test_questions_filter_by_tag(self, client, tag_name, wanted_questions_ids_lst):
+            url = f'/explore/?tag={tag_name}'
+            response = client.get(url)
+
+            requested_questions_ids = response.context['questions'].values_list('id', flat=True)
+            assert wanted_questions_ids_lst == list(requested_questions_ids)
+
+        @pytest.mark.parametrize('tag_name', [
+            "Bagrut_Exam",
+            "5th_Grade",
+            "Pitagoras",
+        ])
+        @pytest.mark.django_db
+        def test_context_with_tags(self, client, tag_name):
+            url = f'/explore/?tag={tag_name}'
+            response = client.get(url)
+
+            requested_tag = response.context['tag']
+            assert requested_tag == tag_name
+
+        @pytest.mark.django_db
+        def test_context_no_tag_parameter(self, explore_page_response):
+            requested_tag = explore_page_response.context['tag']
+            assert requested_tag is None
+
+        @pytest.mark.parametrize('invalid_tag_name', [
+            "AAA",
+            "ERORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR",
+
+        ])
+        @pytest.mark.django_db
+        def test_invalid_tag(self, client, invalid_tag_name):
+            url = f'/explore/?tag={invalid_tag_name}'
+            response = client.get(url)
+            assert response.status_code == 404
