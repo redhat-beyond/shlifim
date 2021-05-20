0cee89f2cae0979b11ebda1d01f405b9fce92871
Ido <idokk96@gmail.com>
2021-05-20 11:24:40 +0300

    change the question model to use the new editor

--- home/models.py
+++ home/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from ckeditor.fields import RichTextField
 from django.contrib.auth.models import User
 from django.http import Http404
-
+from .RichTextBleachField import RichTextBleachField
 
 class Gender(models.TextChoices):
     MALE = 'M', 'Male'
@@ -84,7 +84,7 @@ class Grade(models.TextChoices):
 class Question(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
-    content = RichTextField(blank=True, null=True)
+    content = RichTextBleachField(blank=True, null=True) #RichTextField(blank=True, null=True)
     publish_date = models.DateTimeField(default=timezone.now)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     sub_subject = models.ForeignKey(Sub_Subject, on_delete=models.CASCADE, blank=True, null=True)  # field not required
