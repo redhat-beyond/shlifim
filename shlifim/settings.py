de7db01ed1d8dcc0a3128ba9f443be3a4932df32
liornoy <ironoy@gmail.com>
2021-06-21 18:24:58 +0300

    Init users app
    called startapp users, auto-gen folder and files
    and added the users app to shlifim/settings.py

--- shlifim/settings.py
+++ shlifim/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "home.apps.HomeConfig",
+    "users.apps.UsersConfig",
     "ckeditor",
     'crispy_forms',
 ]
