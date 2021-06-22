423d612aef8fcb2fd73b2125369ec83de83cc594
liornoy <ironoy@gmail.com>
2021-06-22 12:23:49 +0300

    Move users admin from home to users

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import (
-    Profile,
     Subject,
     Sub_Subject,
     Book,
@@ -31,7 +30,6 @@ class AnswerAdmin(admin.ModelAdmin):
     list_display = ("profile", "question", "show_content")
 
 
-admin.site.register(Profile)
 admin.site.register(Subject)
 admin.site.register(Sub_Subject, SubSubjectAdmin)
 admin.site.register(Book, BookAdmin)
